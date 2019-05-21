d65e66e70e9519fbe0296c4fa68f6b3457db1d95
Joanna Hsiao <chsiao58@gmail.com>
2019-05-21 16:09:20 -0400

    Added the solution to Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
-
+    public static void main(){
+        System.out.print('\u000C');             // clear screen
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What's your name?");
+        String name = input.next();
+        
+        if (name.equals("Alice") || name.equals("Bob"))
+            System.out.println("Hey " + name + "!");
+        else
+            System.out.println("You are not Alice or Bob. You are "
+            + name + ".");
+        
     }
 }
